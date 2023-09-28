6e49b2690200239ee4c49b835e3e278e2dd27a81
Jon T <jauntybot.contact@gmail.com>
2023-09-28 12:54:53 -0400

    headbob, infection end

--- Assets/Scripts/First Person Drifter/Optional/HeadBob.cs
+++ Assets/Scripts/First Person Drifter/Optional/HeadBob.cs
@@ -13,41 +13,40 @@ public class HeadBob : MonoBehaviour
 	
 	private float timer = 0.0f; 
  
-	void Update ()
-	{ 
-	    float waveslice = 0.0f; 
-	    float horizontal = Input.GetAxis("Horizontal"); 
-	    float vertical = Input.GetAxis("Vertical"); 
-	    
-	    if (Mathf.Abs(horizontal) == 0f && Mathf.Abs(vertical) == 0f)
-	    { 
-	       timer = 0.0f; 
-	    } 
-	    else
-	    { 
-	       waveslice = Mathf.Sin(timer); 
-	       timer = timer + bobbingSpeed; 
-	       if (timer > Mathf.PI * 2f)
-	       { 
-	          timer = timer - (Mathf.PI * 2f); 
-	       } 
-	    } 
-	    if (waveslice != 0f)
-	    { 
-	       float translateChange = waveslice * bobbingAmount; 
-	       float totalAxes = Mathf.Abs(horizontal) + Mathf.Abs(vertical); 
-	       totalAxes = Mathf.Clamp (totalAxes, 0.0f, 1.0f); 
-	       translateChange = totalAxes * translateChange;
-	       
-	       Vector3 localPos = transform.localPosition;
-	       localPos.y = midpoint + translateChange * Time.timeScale; 
-	       transform.localPosition = localPos;
-	    } 
-	    else
-	    { 
-	    	Vector3 localPos = transform.localPosition;
-	    	localPos.y = midpoint; 
-	    	transform.localPosition = localPos;
-	    } 
+	void Start() {
+		StartCoroutine(BobHead());
+	}
+
+	IEnumerator BobHead () { 
+		while (true) {
+			float waveslice = 0.0f; 
+			float horizontal = Input.GetAxis("Horizontal"); 
+			float vertical = Input.GetAxis("Vertical"); 
+			
+			if (Mathf.Abs(horizontal) == 0f && Mathf.Abs(vertical) == 0f) 
+				timer = 0.0f; 
+
+			else { 
+				waveslice = Mathf.Sin(timer); 
+				timer += Time.deltaTime * bobbingSpeed; 
+				if (timer > Mathf.PI * 2f) { 
+					timer = timer - (Mathf.PI * 2f); 
+				} 
+			} 
+
+			if (waveslice != 0f) { 
+				float translateChange = waveslice * bobbingAmount; 
+				float totalAxes = Mathf.Abs(horizontal) + Mathf.Abs(vertical); 
+				totalAxes = Mathf.Clamp (totalAxes, 0.0f, 1.0f); 
+				translateChange = totalAxes * translateChange;
+				
+				transform.localPosition = new Vector3(transform.localPosition.x, midpoint + translateChange, transform.localPosition.z);
+			} 
+			else
+				transform.localPosition = new Vector3(transform.localPosition.x, midpoint, transform.localPosition.z);
+		
+
+			yield return null;
+		}
 	}
 }
