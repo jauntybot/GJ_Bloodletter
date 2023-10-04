d7ef3f55873ba55953efbe58a6aa9e9f4e624984
Zach Jones <zachonium@gmail.com>
2023-10-04 15:40:37 -0400

    added full screen shader effect structures: blood and infection

---
+++ Assets/Scripts/FullScreenEffectController.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.InputSystem;
+
+public class FullScreenEffectController : MonoBehaviour
+{
+    [Header("Time Stats")]
+    [SerializeField] private float _bloodDisplayTime = 1.5f;
+    [SerializeField] private float _bloodFadeOutTime = 0.5f;
+
+    [Header("Intensity")]
+    [SerializeField] private float _voronoiIntensityStat;
+    [SerializeField] private float _vignetteIntensityStat;
+    
+    [Header("References")]
+    [SerializeField] private ScriptableRendererFeature _fullScreenBlood;
+    [SerializeField] private Material _material;
+
+    private int _voronoiIntensity = Shader.PropertyToID(("_VoronoIntensity"));
+    private int _vignetteIntensity = Shader.PropertyToID(("_VignetteIntensity"));
+
+    private void Start()
+    {
+        _fullScreenBlood.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            StartCoroutine(Hurt());
+        }
+    }
+
+    private IEnumerator Hurt()
+    {
+        _fullScreenBlood.SetActive(true);
+        _material.SetFloat(_voronoiIntensity, _voronoiIntensityStat);
+        _material.SetFloat(_vignetteIntensity, _vignetteIntensityStat);
+
+        float elapsedTime = 0f;
+        while (elapsedTime < _bloodFadeOutTime)
+        {
+            elapsedTime += Time.deltaTime;
+
+            float lerpedVornoi = Mathf.Lerp(0f, _voronoiIntensityStat, (elapsedTime / _bloodFadeOutTime));
+            float lerpedVignette = Mathf.Lerp(0f, _vignetteIntensityStat, (elapsedTime / _bloodFadeOutTime));
+            
+            _material.SetFloat(_voronoiIntensity, lerpedVornoi);
+            _material.SetFloat(_vignetteIntensity, lerpedVignette);
+            
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(_bloodDisplayTime);
+
+        elapsedTime = 0f;
+        while (elapsedTime < _bloodFadeOutTime)
+        {
+            elapsedTime += Time.deltaTime;
+
+            float lerpedVornoi = Mathf.Lerp(_voronoiIntensityStat, 0f, (elapsedTime / _bloodFadeOutTime));
+            float lerpedVignette = Mathf.Lerp(_vignetteIntensityStat, 0f, (elapsedTime / _bloodFadeOutTime));
+            
+            _material.SetFloat(_voronoiIntensity, lerpedVornoi);
+            _material.SetFloat(_vignetteIntensity, lerpedVignette);
+            
+            yield return null;
+        }
+        
+        _fullScreenBlood.SetActive(false);
+    }
+}
