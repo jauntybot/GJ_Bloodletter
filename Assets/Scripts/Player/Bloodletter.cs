ef8aeac5606a32e2fb595eb8b5e8e5a64513bb27
Jon T <jauntybot.contact@gmail.com>
2023-10-21 13:39:18 -0400

    build ready

--- Assets/Scripts/Player/Bloodletter.cs
+++ Assets/Scripts/Player/Bloodletter.cs
@@ -2,9 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
-using System.Threading;
-using UnityEngine.Rendering.Universal;
-using UnityEditor.UIElements;
 using UnityEngine.Playables;
 
 [RequireComponent(typeof(PlayableDirector))]
