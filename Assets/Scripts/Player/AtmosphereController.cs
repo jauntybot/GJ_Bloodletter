3505d0511b36c4b14775a8ecce97de8e744c4adc
Jon T <jauntybot.contact@gmail.com>
2023-10-27 11:09:08 -0400

    darkness linked to enemy proximity during chase

--- Assets/Scripts/Player/AtmosphereController.cs
+++ Assets/Scripts/Player/AtmosphereController.cs
@@ -7,27 +7,36 @@ public class AtmosphereController : MonoBehaviour
 {
 
     Bloodletter bloodletter;
+    EnemyPathfinding enemy;
 
     [SerializeField] Material skyboxMat;
     [SerializeField] Light pointLight;
     [SerializeField] Vector2 pointLightRange, skyboxExposureRange;
     [SerializeField] Color fogStart;
 
+    [SerializeField] float falloffDist, effectAmp, effectFreq;
+
+
 
     void Start() {
         bloodletter = GetComponent<Bloodletter>();
+        enemy = EnemyPathfinding.instance;
     }
 
 
     void Update() {
 
-        skyboxMat.SetFloat("_Exposure", Mathf.Lerp(skyboxExposureRange.x, skyboxExposureRange.y, Mathf.InverseLerp(0, 100, bloodletter.infectionLevel)));
-        pointLight.intensity = Mathf.Lerp(pointLightRange.x, pointLightRange.y, Mathf.InverseLerp(0, 100, bloodletter.infectionLevel));
-        RenderSettings.fogColor = Color.Lerp(fogStart, Color.black, bloodletter.infectionLevel / 100);
-
-
-
-
+        if (enemy.state == EnemyPathfinding.EnemyState.Chasing) {
+            float strength = (falloffDist - Vector3.Distance(enemy.transform.position, bloodletter.transform.position)) / falloffDist;
+            float sinMod = Mathf.Sin(Time.time * effectFreq) * effectAmp;
+            skyboxMat.SetFloat("_Exposure", Mathf.Lerp(skyboxExposureRange.x, skyboxExposureRange.y, Mathf.InverseLerp(0, falloffDist, sinMod * (falloffDist - Vector3.Distance(enemy.transform.position, bloodletter.transform.position)))));
+            pointLight.intensity = Mathf.Lerp(pointLightRange.x, pointLightRange.y, Mathf.InverseLerp(0, 100, sinMod * (falloffDist - Vector3.Distance(enemy.transform.position, bloodletter.transform.position))));
+            RenderSettings.fogColor = Color.Lerp(fogStart, Color.black, sinMod * strength);
+        } else {
+            skyboxMat.SetFloat("_Exposure", skyboxExposureRange.x);
+            pointLight.intensity = pointLightRange.x;
+            RenderSettings.fogColor = fogStart;
+        }
     }
 
 }
